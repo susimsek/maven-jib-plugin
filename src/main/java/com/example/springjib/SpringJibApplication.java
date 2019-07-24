ac2e7c196b54adca35cdbd1463f60c059f98e6ea
suayb <suaybsimsek58@gmail.com>
2019-07-24 15:14:17 +0300

    add HelloWorld Controller

---
+++ src/main/java/com/example/springjib/SpringJibApplication.java
@@ -0,0 +1,13 @@
+package com.example.springjib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJibApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJibApplication.class, args);
+    }
+
+}
